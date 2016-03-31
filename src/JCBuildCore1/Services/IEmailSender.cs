dc97f81bd99ec9a90cd2c89034965a22b30cf56a
joncoello <joncoello@gmail.com>
2016-03-31 12:04:53 -0700

    initial commit of web project

---
+++ src/JCBuildCore1/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JCBuildCore1.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
