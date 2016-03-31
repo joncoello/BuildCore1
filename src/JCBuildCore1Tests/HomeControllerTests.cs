7e032a0fe713421b12e10bc1187b886a49783603
joncoello <joncoello@gmail.com>
2016-03-31 12:31:51 -0700

    add test

---
+++ src/JCBuildCore1Tests/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using JCBuildCore1.Controllers;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JCBuildCore1Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class HomeControllerTests
+    {
+        [Fact()]
+        public void HomeIndexReturnsView() {
+
+            // assemble
+            var controller = new HomeController();
+
+            //act
+            var result = controller.Index() as ViewResult;
+
+            //assert
+            Assert.NotNull(result);
+
+        }
+    }
+}
---
+++ src/JCBuildCore1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JCBuildCore1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JCBuildCore1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5b40f17-ad9c-4581-b7cc-9769830414c9")]
